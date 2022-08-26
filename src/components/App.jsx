3415d98d1a15211e40866d3dcef5646c11e4c0ac
Muhammad Zain-ul-abideen <zaynzafar18@gmail.com>
2022-08-26 18:02:42 +0500

    Initial Commit

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Footer from './footer/Footer'
+import Header from './header/Header'
+import Note from './note/Note';
+import './App.css';
+
+const App = () => {
+  return (
+    <>
+      <Header  />
+      <Note  />  
+      <Footer  />
+    </>
+  )
+}
+
+export default App
