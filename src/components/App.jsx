43f694694c35f2205e7276f26f09c6f4742f78eb
Muhammad Zain-ul-abideen <zaynzafar18@gmail.com>
2022-09-01 16:51:29 +0500

    change css to scss

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Footer from './footer/Footer'
 import Header from './header/Header'
 import Note from './note/Note';
-import './App.css';
+import './App.scss';
 import notes from '../note';
 
  
--- src/components/note/Note.jsx
+++ src/components/note/Note.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Note.css'
+import './Note.scss'
 
 const Note = (props) => {
   return (
